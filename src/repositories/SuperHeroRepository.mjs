ad4aeb48b45e6593ec3726187270015c6f2a65d2
enzoramos597 <enzo.j3.ramos@gmail.com>
2025-03-23 19:57:26 -0300

    Funcionando con mayores de edad

--- src/repositories/SuperHeroRepository.mjs
+++ src/repositories/SuperHeroRepository.mjs
@@ -12,7 +12,7 @@ class SuperHeroRepository extends IRepository{
     }
 
     async obtenerMayoresDe30() {
-        return await superHero.find({ edad: { $gt: 30 } });
+        return await superHero.find({ edad: { $gt: 30 }, planetaOrigen: "Tierra", poderes: { $exists: true, $not: { $size: 1 } }});
     }
 
     /*async obtenerMenoresDe30() {
